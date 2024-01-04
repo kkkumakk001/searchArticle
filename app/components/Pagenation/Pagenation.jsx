2566fa23e0cc4bf8f815d26d000e0f0e25068fde
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-04 16:47:37 +0900

    router.pushに渡すパラメータの変更
    pageNumberだとレンダリング前で数値が変更されておらず反映が1つ遅れるので、useStateは使用せずに数値型に変更してから指定することにした

--- app/components/Pagenation/Pagenation.jsx
+++ app/components/Pagenation/Pagenation.jsx
@@ -1,28 +1,25 @@
 "use client"
-import { React, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import Pagination from '@mui/material/Pagination';
 import { useRouter } from "next/navigation";
 import Stack from '@mui/material/Stack';
 import Articles from '../Articles/Articles';
-// import style from "./PagenationButton.module.scss"
 
-export default function Pagenation({ propspageNumber }) {
+export default function Pagenation({ paramsId, sort }) {
   const router = useRouter();
-  const [pageNumber, setPageNumber] = useState(propspageNumber);
+  const [pageNumber, setPageNumber] = useState(paramsId);
 
-  const changeType = (e) => {
+  const routerPush = (e) => {
     const num = Number(e.target.textContent);
-    setPageNumber(num);
-    router.push("/article/popular/${pageNumber}")
-
+    router.push(`/article/popular/${num}`);
   }
 
   return (
     <>
-    <Articles pageNumber={pageNumber} sort="popular"></Articles>
+    <Articles pageNumber={pageNumber} sort={sort}></Articles>
     <div className="pagenation">
     <Stack spacing={2}>
-      <Pagination count={10} page={pageNumber} variant="outlined" shape="rounded" onClick={(e)=>changeType(e)} hidePrevButton hideNextButton />
+      <Pagination count={100} page={pageNumber} variant="outlined" shape="rounded" onClick={(e)=>routerPush(e)} hidePrevButton hideNextButton />
     </Stack>
     </div>
     </>
