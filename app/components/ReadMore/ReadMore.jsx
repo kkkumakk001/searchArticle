d7369bdc58e8ee5fdb36c297a12cbd9476bb20f7
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-04 16:27:06 +0900

    ReadMoreファイルの作成
    記事一覧の一部を表示し、続きを見るをクリックしたら記事一覧ページに遷移させる

---
+++ app/components/ReadMore/ReadMore.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useRef } from "react";
+import classes from "./ReadMore.module.scss";
+import Articles from "../Articles/Articles"
+
+export default function ReadMore({sort}) {
+  const readMoreButton = useRef();
+  return (
+    <div className={classes.readmore}>
+      <input id="readmore" type="checkbox" ref={readMoreButton}></input>
+      <label htmlFor="readmore">続きを見る</label>
+          <div className={classes.readmoreContent}>
+              <Articles sort={sort}></Articles>
+      </div>
+    </div>
+  );
+}
