f3ee5ced5ad4ef6d7dfab29f071233be60a388f4
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-04 16:30:32 +0900

    ReadMoreコンポーンネントの読み込み

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
 "use client"
+import ReadMore from "./components/ReadMore/ReadMore"
 
 export default function Top() {
   return (
     <>
-      <div style={{fontSize: "32px"}}>Home Page</div>
+      <div style={{ fontSize: "32px" }}>Home Page</div>
+      <p>新着記事</p>
+      <ReadMore></ReadMore>
+      <p>人気記事</p>
+      <ReadMore sort="popular"></ReadMore>
     </>
   );
 }
