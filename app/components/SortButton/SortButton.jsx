3d4b2a557f1d6de9e0d6fcdd15eb4ad98c039620
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-05 16:19:59 +0900

    カレントページに対応するボタンのCSSの出し分け
    usePathnameを使用し、popularページまたはrecentページによって切り替えボタンのCSSを変化させた

--- app/components/SortButton/SortButton.jsx
+++ app/components/SortButton/SortButton.jsx
@@ -1,12 +1,24 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
+import style from "./SortButton.module.scss";
 
 export default function SortButton() {
   const router = useRouter();
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
-    <div>
-      <button onClick={() => router.push("/article/recent/01")}>新着順</button>
-      <button onClick={() => router.push("/article/popular/01")}>人気順</button>
-    </div>
+    <section className={style.buttonSection}>
+      <div className={style.buttonWrapper}>
+        <button
+          className={`${style.button} ${pathname.includes("recent") ? style.currentButton : style.nonCurrentButton}`}
+          onClick={() => router.push("/article/recent/01")}>
+          新着記事
+        </button>
+        <button
+          className={`${style.button} ${pathname.includes("popular") ? style.currentButton : style.nonCurrentButton}`}
+          onClick={() => router.push("/article/popular/01")}>人気記事</button>
+      </div>
+    </section>
   );
 }
