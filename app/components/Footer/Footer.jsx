6e63442d06619e28370d742fb6e77501485ae9d4
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-06 17:41:51 +0900

    ヘッダーのデザイン当て
    要素
    - サイトロゴ
    - SNSロゴ
    - aboutページへの誘導
    - コピーライト
    
    後から各SNSのURLを追加する

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,24 +1,86 @@
 "use client";
 import { useRouter } from "next/navigation";
 import style from "./Footer.module.scss";
+import Image from "next/image";
+import siteLogo from "../../../public/images/siteLogo.jpg"
+import xIcon from "../../../public/images/xIcon.png"
+import qiitaIcon from "../../../public/images/qiitaIcon.png"
+import githubIcon from "../../../public/images/githubIcon.png"
+import noteIcon from "../../../public/images/noteIcon.png"
+import zennIcon from "../../../public/images/_ zennIcon.png"
+
 
 
 export default function FooterComponent() {
 const router = useRouter();
   return (
-      <>
-          <header>
-              <nav>
-                  <ul>
-                      <li className={style.listItem} onClick={()=>router.push("/")}>Home</li>
-                      <li className={style.listItem} onClick={()=>router.push("/about")}>About</li>
-                      <li className={style.listItem} onClick={()=>router.push("/category")}>Category</li>
-                      <li className={style.listItem} onClick={()=>router.push("/article/popular/01")}>Article/Popular</li>
-                      <li className={style.listItem} onClick={()=>router.push("/article/recent/01")}>Article/Recent</li>
-                      <li className={style.listItem} onClick={()=>router.push("/dashboard/bookmark")}>Dashboard/Bookmark</li>
-                  </ul>
-              </nav>
-          </header>
-      </>
+      <footer className={style.footer}>
+            <nav className={style.socialIcons}>
+                <Image
+                    className={style.siteLogo}
+                    src={siteLogo}
+                    alt="サイトロゴ"
+                    height={32}
+                    width={32}
+                    quality={90}
+                    priority={true}
+                    onClick={()=>router.push("/")}
+                ></Image>
+                <ul className={style.socialIcons_ul}>
+                    <li className={style.socialIcons_li}><Image
+                        className={style.socialIcons_icon}
+                            src={xIcon}
+                            alt="Xのアイコン"
+                            height={24}
+                            width={24}
+                            quality={90}
+                            priority={true}
+                            onClick={()=>router.push("#")}
+                        ></Image></li>
+                        <li className={style.socialIcons_li}><Image
+                            className={style.socialIcons_icon}
+                                src={githubIcon}
+                                alt="githubのアイコン"
+                                height={24}
+                                width={24}
+                                quality={90}
+                                priority={true}
+                                onClick={()=>router.push("#")}
+                            ></Image></li>
+                    <li className={style.socialIcons_li}><Image
+                        className={style.socialIcons_icon}
+                            src={qiitaIcon}
+                            alt="Qiitaのアイコン"
+                            height={24}
+                            width={24}
+                            quality={90}
+                            priority={true}
+                            onClick={()=>router.push("#")}
+                        ></Image></li>
+                    <li className={style.socialIcons_li}><Image
+                        className={style.socialIcons_icon}
+                            src={noteIcon}
+                            alt="noteのアイコン"
+                            height={24}
+                            width={24}
+                            quality={90}
+                            priority={true}
+                            onClick={()=>router.push("#")}
+                        ></Image></li>
+                    <li className={style.socialIcons_li}><Image
+                        className={style.socialIcons_icon}
+                            src={zennIcon}
+                            alt="zennのアイコン"
+                            height={24}
+                            width={24}
+                            quality={90}
+                            priority={true}
+                            onClick={()=>router.push("#")}
+                        ></Image></li>
+                </ul>
+              <p className={style.siteInfo} onClick={()=>router.push("/about")}>本サイトについて</p>
+              <p>&copy; @kkkumakk001(Github)</p>
+            </nav>
+      </footer>
   )
 }
