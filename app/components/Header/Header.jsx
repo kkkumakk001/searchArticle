22840dc3b9cb519a9be26e4a01c2b4c3aab70b26
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-06 15:43:40 +0900

    ヘッダーのスタイル当て、アイコンの読み込み
    要素
    - サイトロゴ
    - 検索ボタン
    - 未ログインならログインボタン
    - ログイン済みならユーザーアイコン

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,15 +1,19 @@
 "use client";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import style from "./Header.module.scss";
 import Image from "next/image";
 import siteLogo from "../../../public/images/siteLogo.jpg"
+import searchIcon from "../../../public/images/searchIcon.png"
+import userIcon from "../../../public/images/userIcon.png"
 
 
 export default function HeaderComponent() {
-    const router = useRouter();
+  const router = useRouter();
+  const [authInfo, setAuthInfo] = useState(false);
   return (
-        <header>
-            <nav>
+        <header className={style.header}>
+            <nav className={style.nav}>
               <Image
                   src={siteLogo}
                   alt="サイトロゴ"
@@ -19,14 +23,39 @@ export default function HeaderComponent() {
                   priority={true}
                   onClick={()=>router.push("/")}
               ></Image>
-                <ul>
-                    <li className={style.listItem} onClick={()=>router.push("/")}>Home</li>
-                    <li className={style.listItem} onClick={()=>router.push("/about")}>About</li>
-                    <li className={style.listItem} onClick={()=>router.push("/category")}>Category</li>
-                    <li className={style.listItem} onClick={()=>router.push("/article/popular/01")}>Article/Popular</li>
-                    <li className={style.listItem} onClick={()=>router.push("/article/recent/01")}>Article/Recent</li>
-                    <li className={style.listItem} onClick={()=>router.push("/dashboard/bookmark")}>Dashboard/Bookmark</li>
-                </ul>
+              <span className={style.header_info}>
+              <Image
+                  className={style.searchIcon}
+                  src={searchIcon}
+                  alt="検索アイコン"
+                  height={24}
+                  width={24}
+                  quality={90}
+                  priority={true}
+                  onClick={()=>router.push("/category")}
+              ></Image>
+              {authInfo ? (
+                <Image
+                  src={userIcon}
+                  alt="ユーザーアイコン"
+                  height={32}
+                  width={32}
+                  quality={90}
+                  priority={true}
+                  onClick={()=>router.push("/dashboard")}
+              ></Image>
+              ) : (<button className={style.loginButton}>ログイン</button>)
+              }
+              </span>
+              
+                {/* <ul>
+                //     <li className={style.listItem} onClick={()=>router.push("/")}>Home</li>
+                //     <li className={style.listItem} onClick={()=>router.push("/about")}>About</li>
+                //     <li className={style.listItem} onClick={()=>router.push("/category")}>Category</li>
+                //     <li className={style.listItem} onClick={()=>router.push("/article/popular/01")}>Article/Popular</li>
+                //     <li className={style.listItem} onClick={()=>router.push("/article/recent/01")}>Article/Recent</li>
+                //     <li className={style.listItem} onClick={()=>router.push("/dashboard/bookmark")}>Dashboard/Bookmark</li>
+                </ul> */}
             </nav>
         </header>
   )
