f0bc9099e07177224829b793ac68b26cbf79de8d
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-06 11:22:27 +0900

    記事一覧ページへのリンクの追加

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,16 @@
 "use client"
+import { useRouter } from "next/navigation";
+import style from "./styles/TopPage.module.scss"
 import ReadMore from "./components/ReadMore/ReadMore"
 
 export default function Top() {
+  const router = useRouter();
   return (
-    <>
-      <div style={{ fontSize: "32px" }}>Home Page</div>
-      <p>新着記事</p>
-      <ReadMore></ReadMore>
-      <p>人気記事</p>
+    <main className={style.mainContainer}>
+      <h2 className={style.articleNav} onClick={() => router.push("/article/recent/01")}>新着記事</h2>
+      <ReadMore style={{marginButtom: "100px"}}></ReadMore>
+      <h2 className={style.articleNav} onClick={() => router.push("/article/popular/01")}>人気記事</h2>
       <ReadMore sort="popular"></ReadMore>
-    </>
+    </main>
   );
 }
