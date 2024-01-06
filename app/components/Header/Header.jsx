55be364e07aec580521919b54d36c7bc4c171db5
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-06 11:21:14 +0900

    サイトロゴの読み込み、ヘッダーへの追加

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,24 +1,33 @@
 "use client";
 import { useRouter } from "next/navigation";
 import style from "./Header.module.scss";
+import Image from "next/image";
+import siteLogo from "../../../public/images/siteLogo.jpg"
 
 
 export default function HeaderComponent() {
     const router = useRouter();
   return (
-      <>
-          <header>
-              <nav>
-                  <ul>
-                      <li className={style.listItem} onClick={()=>router.push("/")}>Home</li>
-                      <li className={style.listItem} onClick={()=>router.push("/about")}>About</li>
-                      <li className={style.listItem} onClick={()=>router.push("/category")}>Category</li>
-                      <li className={style.listItem} onClick={()=>router.push("/article/popular/01")}>Article/Popular</li>
-                      <li className={style.listItem} onClick={()=>router.push("/article/recent/01")}>Article/Recent</li>
-                      <li className={style.listItem} onClick={()=>router.push("/dashboard/bookmark")}>Dashboard/Bookmark</li>
-                  </ul>
-              </nav>
-          </header>
-      </>
+        <header>
+            <nav>
+              <Image
+                  src={siteLogo}
+                  alt="サイトロゴ"
+                  height={32}
+                  width={32}
+                  quality={90}
+                  priority={true}
+                  onClick={()=>router.push("/")}
+              ></Image>
+                <ul>
+                    <li className={style.listItem} onClick={()=>router.push("/")}>Home</li>
+                    <li className={style.listItem} onClick={()=>router.push("/about")}>About</li>
+                    <li className={style.listItem} onClick={()=>router.push("/category")}>Category</li>
+                    <li className={style.listItem} onClick={()=>router.push("/article/popular/01")}>Article/Popular</li>
+                    <li className={style.listItem} onClick={()=>router.push("/article/recent/01")}>Article/Recent</li>
+                    <li className={style.listItem} onClick={()=>router.push("/dashboard/bookmark")}>Dashboard/Bookmark</li>
+                </ul>
+            </nav>
+        </header>
   )
 }
