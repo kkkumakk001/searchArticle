ed9ec634250c3f0b25321fb5aa8c2032016f82ca
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-09 13:57:56 +0900

    ReadMoreの高さをpropsで受け取るようにした
    記事一覧とカテゴリ一覧で表示する高さを変えたくなったのでpropsでheightを受け取ることにした

--- app/components/ReadMore/ReadMore.jsx
+++ app/components/ReadMore/ReadMore.jsx
@@ -1,20 +1,16 @@
 "use client";
-
 import { useRef } from "react";
 import classes from "./ReadMore.module.scss";
-import Articles from "../Articles/Articles"
 
-// export default function ReadMore({sort}) {
-export default function ReadMore({children}) {
+export default function ReadMore({children, height}) {
   const readMoreButton = useRef();
   return (
     <div className={classes.readmore}>
       <input id="readmore" type="checkbox" ref={readMoreButton}></input>
       <label htmlFor="readmore">続きを見る</label>
-          <div className={classes.readmoreContent}>
-        {/* <Articles sort={sort}></Articles> */}
-            { children }
-          </div>
+      <div className={classes.readmoreContent} style={{height: height}}>
+        { children }
+      </div>
     </div>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -11,10 +11,11 @@ export default function Top() {
   return (
     <main className={style.mainContainer}>
       <h2 className={style.articleNav} onClick={() => router.push("/article/recent/01")}>新着記事</h2>
-      <ReadMore><Articles sort="recent"></Articles></ReadMore>
+      <ReadMore height="600px"><Articles sort="recent"></Articles></ReadMore>
       <h2 className={style.articleNav} onClick={() => router.push("/article/popular/01")}>人気記事</h2>
-      <ReadMore><Articles sort="popular"></Articles></ReadMore>
-      <Categorys></Categorys>
+      <ReadMore height="600px"><Articles sort="popular"></Articles></ReadMore>
+      <h2 className={style.articleNav} onClick={() => router.push("/category")}>人気記事</h2>
+      <ReadMore height="400px"><Categorys></Categorys></ReadMore>
     </main>
   );
 }
