5f7705c474af9c589711663cc1cb4203a76b872a
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-11 17:27:14 +0900

    カテゴリ一覧ページを実装、Imageに外部ドメインを使用する際の設定
    最大6列、最小2列
    
    Imageに外部ドメインの画像をしようするとエラーが出るので
    
    const nextConfig = {
      images: {
        domains: ['images.dog.ceo'], //ここにドメインを指定
      },
    }
    
    これを追記することで解決できた

--- app/category/page.jsx
+++ app/category/page.jsx
@@ -1,12 +1,34 @@
-"use client";
-import { usePathname } from "next/navigation";
+'use client'
+import categoryDatas from "../Datas/categoryDatas";
+import style from "./page.module.scss"
+import Image from "next/image";
 
 export default function CategoryAll() {
-    const pathName = usePathname();
-
+  const { programmingLanguages, framework, other } = categoryDatas();
+  const categoryAll = [...programmingLanguages, ...framework, ...other];
+  
   return (
-    <div>
-          <p>{pathName}</p>
+      <div className={style.category_page}>
+        <h2 className={style.category_page_title}>カテゴリ一覧</h2>
+        <ul className={style.ul}>
+            {categoryAll.map((data, index) => {
+                return (
+                    <li className={style.li} key={index}>
+                        <Image
+                            // className={style.siteLogo}
+                            src={data.src}
+                            alt="サイトロゴ"
+                            height={50}
+                            width={50}
+                            quality={90}
+                            priority={true}
+                            // onClick={()=>router.push("/")}
+                        ></Image>
+                        <p className={style.li_name}>{ data.name }</p>    
+                    </li>
+                )
+            })}   
+        </ul>    
     </div>
   )
 }
