3eff774736cd4dfade97f1c2e4b6be4c1121904c
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-12 15:01:32 +0900

    aboutページのスタイル当て
    余計なアニメーションや凝った構成にはせず、サイトの使い方を理解してもらうことを重視した。
    
    開けているスペースにGIFで実際にアプリを使用しているようすを載せる

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,12 +1,48 @@
-"use client";
-import { usePathname } from "next/navigation";
+import style from "./page.module.scss"
 
 export default function About() {
-    const pathName = usePathname();
+
+    const datas = [
+      {
+        mainTitle: "記事検索",
+        subTitle: "Search-Article",
+        text: "トレンドや新着の記事、タグ検索、任意のワード検索など基本的な機能を用意しています。"
+      },
+      {
+        mainTitle: "ブックマーク、メモ",
+        subTitle: "Bookmark & Memo",
+        text: "ブックマーク機能だけではなく、同時にメモも記しておくことで振り返りが楽に。"
+      },
+      {
+        mainTitle: "絞り込み",
+        subTitle: "Sort",
+        text: "ブックマークが増えてもすぐに探している記事が見つかるように、タグでの絞り込みができるようにしました。"
+      }
+    ];
 
   return (
-    <div>
-          <p>{pathName}</p>
+    <div className={style.about_page} style={{paddingTop: "150px"}}>
+          <ul className={style.ul}>
+              {datas.map((data, index) => {
+                return (
+                <li className={style.li}>
+                    <div className={style.li_content}>
+                      <div className={style.li_title}>
+                        <div className={style.li_number}><span>{`0${index + 1}`}</span></div> 
+                        <div className={style.li_titleText}>
+                            <p className={style.li_titleText_main}>{data.mainTitle}</p>   
+                            <p className={style.li_titleText_sub}>{data.subTitle}</p>   
+                        </div> 
+                      </div>
+                      <div className={style.li_text}>
+                        <p>{data.text}</p>
+                      </div>
+                    </div>
+                    <div className={style.li_img}></div>    
+                </li>
+                )
+              })}
+        </ul>
     </div>
   )
 }
