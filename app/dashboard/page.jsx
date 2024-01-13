79b5a4263d996efe1e6ae3173f07e4abed956ea8
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-13 16:35:22 +0900

    ダッシュボードページのスタイル当て
    Firebase連携後ブックマーク下にブックマークした記事を表示する。
    
    タグ検索で記事を絞り込めるようにする。

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,12 +1,58 @@
-"use client";
-import { usePathname } from "next/navigation";
+"use client"
+import { useState } from "react";
+import Image from "next/image"
+import style from "./page.module.scss"
+import NoImage from "../../public/images/NoImage.png"
+import UserIcon from "../../public/images/userIcon.svg"
 
 export default function Dashboard() {
-    const pathName = usePathname();
+  const [authInfo, setAuthInfo] = useState(true);
+  const logout = () => alert("logout");
 
   return (
-    <div>
-          <p>{pathName}</p>
+    <div className={style.dashboard_page}>
+      <div className={style.dashboard_wrapper}>
+        <div className={style.userInformation}>
+          <div className={style.userIcon_block}>
+            {authInfo ? (
+              <Image
+                className={style.userIcon}
+                src={UserIcon}
+                alt="ユーザーアイコン"
+                height={200}
+                width={200}
+                quality={90}
+                priority={true}
+              ></Image>
+            ) : (
+              <Image
+                className={style.userIcon}
+                src={NoImage}
+                alt="ユーザーアイコンが見つかりませんでした"
+                height={200}
+                width={200}
+                quality={90}
+                priority={true}
+              ></Image> 
+            )}
+          </div>
+          <div className={style.userInfo}>
+            <p className={style.userName}>UserName</p>
+            <p className={style.userId}>@UserId</p>
+            <button className={style.logoutButton} onClick={logout}>ログアウト</button>
+          </div>
+        </div>
+        <div className={style.bookmarks}>
+          <div className={style.bookmarks_head}>
+            <p className={style.bookmarks_title}>ブックマーク</p>
+            <div className={style.bookmarks_sort_wrapper}>
+              <label className={style.bookmarks_sort_label}>
+                <input className={style.bookmarks_sort} type="text" placeholder="検索"/>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
