e7141de5e78babafd9892c698a54bdfb5a5ebb2e
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-17 12:41:14 +0900

    サイドバーのタグ一覧にページ遷移イベントを追加

--- app/components/Sidebar/Sidebar.jsx
+++ app/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,21 @@
+"use client"
+
 import React from 'react'
+import { useRouter } from "next/navigation"
+import { useTagUpdateContext } from "../../context/TagContext"
 import style from "./Sidebar.module.scss"
 import categoryDatas from "../../Datas/categoryDatas"
 
 export default function Sidebar() {
+  const setTag = useTagUpdateContext()
+  const router = useRouter()
   const { programmingLanguages, framework, other } = categoryDatas();
-
+  const goTagPage = (data) => {
+        const tagName = data.name
+        setTag(tagName);
+        router.push(`/category/${data.name}/popular/01`)
+    }
+    
   return (
     <aside className="sidebar">
         <nav className={style.nav}>
@@ -12,7 +23,7 @@ export default function Sidebar() {
                 <p className={style.nav_ul_title}>プログラミング言語</p>
                 {programmingLanguages.map((data, index) => {
                     return (
-                        <li className={style.nav_li} key={index}>#{ data.name }</li>
+                        <li className={style.nav_li} key={index} onClick={() => {goTagPage(data)}}>#{ data.name }</li>
                     )
                 })}
             </ul>
@@ -20,7 +31,7 @@ export default function Sidebar() {
                 <p className={style.nav_ul_title}>フレームワーク</p>
                 {framework.map((data, index) => {
                     return (
-                        <li className={style.nav_li} key={index}>#{ data.name }</li>
+                        <li className={style.nav_li} key={index} onClick={() => {goTagPage(data)}}>#{ data.name }</li>
                     )
                 })}
             </ul>
@@ -28,7 +39,7 @@ export default function Sidebar() {
                 <p className={style.nav_ul_title}>その他</p>
                 {other.map((data, index) => {
                     return (
-                        <li className={style.nav_li} key={index}>#{ data.name }</li>
+                        <li className={style.nav_li} key={index} onClick={() => {goTagPage(data)}}>#{ data.name }</li>
                     )
                 })}
             </ul>
