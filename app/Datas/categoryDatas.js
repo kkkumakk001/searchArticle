f795201fe682f43f133ac465233cc14b0a23ded3
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-01-30 17:44:55 +0900

    flexbox利用時、最終行のズレを修正

--- app/Datas/categoryDatas.js
+++ app/Datas/categoryDatas.js
@@ -1,60 +1,117 @@
 export default function categoryDatas() {
 
     const programmingLanguages = [
-        { name: "C", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" },
-        { name: "Java", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" },
-        { name: "JavaScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
-        { name: "PHP", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" },
-        { name: "Ruby", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg" },
-        { name: "TypeScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" },
-        { name: "Python", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
-        { name: "R", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/r/r-original.svg" },
-        { name: "Go", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" },
-        { name: "Swift", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg" },
-        { name: "Kotlin", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg" },
-        { name: "Objective-C", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/objectivec/objectivec-plain.svg" },
-        { name: "Haskell", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/haskell/haskell-original.svg" },
-        { name: "Scala", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/scala/scala-original.svg" },
-        { name: "Groovy", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/groovy/groovy-original.svg" },
-        { name: "Dart", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" },
-        { name: "Perl", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/perl/perl-original.svg" },
-        { name: "MATLAB", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/matlab/matlab-original.svg" }
+    
+        { name: "C", src: "images/programmingLanguages/c.svg" },
+        { name: "Java", src: "images/programmingLanguages/java.svg"},
+        { name: "JavaScript", src: "images/programmingLanguages/javascript.svg" },
+        { name: "PHP", src: "images/programmingLanguages/php.svg" },
+        { name: "Ruby", src: "images/programmingLanguages/ruby.svg" },
+        { name: "TypeScript", src: "images/programmingLanguages/typescript.svg" },
+        { name: "Python", src: "images/programmingLanguages/python.svg" },
+        { name: "R", src: "images/programmingLanguages/r-lang.svg" },
+        { name: "Go", src: "images/programmingLanguages/go.svg" },
+        { name: "Swift", src: "images/programmingLanguages/swift.svg" },
+        { name: "Kotlin", src: "images/programmingLanguages/kotlin.svg" },
+        { name: "Haskell", src: "images/programmingLanguages/haskell.svg" },
+        { name: "Scala", src: "images/programmingLanguages/scala.svg" },
+        { name: "Dart", src: "images/programmingLanguages/dart.svg" },
+        { name: "Perl", src: "images/programmingLanguages/perl.svg" },
+        { name: "Groovy", src: "images/programmingLanguages/groovy.svg" },
+        { name: "Objective-C", src: "images/programmingLanguages/objective-c.svg" },
+        { name: "MATLAB", src: "images/programmingLanguages/matlab.svg" }
     ];
 
     const framework = [
-        { name: "Ruby on Rails", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg" },
-        { name: "Laravel", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg" },
-        { name: "CakePHP", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cakephp/cakephp-original.svg" },
-        { name: "AngularJS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" },
-        { name: "Vue.js", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg" },
-        { name: "React", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-        { name: "Bootstrap", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" },
-        { name: "Foundation", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/foundation/foundation-original.svg" },
-        { name: "Django", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" },
-        { name: "Spring", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" },
-        { name: "Gatsby", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gatsby/gatsby-plain.svg" },
-        { name: "Next.js", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
-        { name: "Nuxt.js", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg" },
-        { name: "Express", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" },
-        { name: "Flutter", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" }
+        { name: "Ruby on Rails", src: "images/framework/rails.svg" },
+        { name: "Laravel", src: "images/framework/laravel.svg" },
+        { name: "CakePHP", src: "images/framework/cakephp.svg" },
+        { name: "AngularJS", src: "images/framework/angular.svg" },
+        { name: "Vue.js", src: "images/framework/vue.svg" },
+        { name: "React", src: "images/framework/react.svg" },
+        { name: "Bootstrap", src: "images/framework/bootstrap.svg" },
+        { name: "Foundation", src: "images/framework/foundation.svg" },
+        { name: "Django", src: "images/framework/django.svg" },
+        { name: "Spring", src: "images/framework/spring.svg" },
+        { name: "Gatsby", src: "images/framework/gatsby.svg" },
+        { name: "Next.js", src: "images/framework/nextjs.svg" },
+        { name: "Nuxt.js", src: "images/framework/nuxt.svg" },
+        { name: "Express", src: "images/framework/express.svg" },
+        { name: "Flutter", src: "images/framework/flutter.svg" }
     ];
 
     const other = [
-        { name: "PostgreSQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" },
-        { name: "MySQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" },
-        { name: "mongoDB", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" },
-        { name: "Heroku", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" },
-        { name: "Firebase", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" },
-        { name: "Github", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.sv" },
-        { name: "Docker", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" },
-        { name: "Slack", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-original.svg" },
-        { name: "HTML", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
-        { name: "CSS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
-        { name: "SASS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" },
-        { name: "atom", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/atom/atom-original.svg" },
-        { name: "babel", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/babel/babel-original.svg" },
-        { name: "linux", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" }
+        { name: "PostgreSQL", src: "images/other/postgresql.svg" },
+        { name: "MySQL", src: "images/other/mysql.svg" },
+        { name: "mongoDB", src: "images/other/mongodb.svg" },
+        { name: "Heroku", src: "images/other/heroku.svg" },
+        { name: "Firebase", src: "images/other/firebase.svg" },
+        { name: "Github", src: "images/other/github.svg" },
+        { name: "Docker", src: "images/other/docker.svg" },
+        { name: "Slack", src: "images/other/slack.svg" },
+        { name: "HTML", src: "images/other/html.svg" },
+        { name: "CSS", src: "images/other/css.svg" },
+        { name: "SASS", src: "images/other/sass.svg" },
+        { name: "atom", src: "images/other/atom.svg" },
+        { name: "babel", src: "images/other/babel.svg" },
+        { name: "linux", src: "images/other/linux.svg" }
     ];
 
+
+    //     const programmingLanguages = [
+    //     { name: "C", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" },
+    //     { name: "Java", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" },
+    //     { name: "JavaScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+    //     { name: "PHP", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" },
+    //     { name: "Ruby", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg" },
+    //     { name: "TypeScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" },
+    //     { name: "Python", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
+    //     { name: "R", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/r/r-original.svg" },
+    //     { name: "Go", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" },
+    //     { name: "Swift", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg" },
+    //     { name: "Kotlin", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg" },
+    //     { name: "Objective-C", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/objectivec/objectivec-plain.svg" },
+    //     { name: "Haskell", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/haskell/haskell-original.svg" },
+    //     { name: "Scala", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/scala/scala-original.svg" },
+    //     { name: "Groovy", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/groovy/groovy-original.svg" },
+    //     { name: "Dart", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" },
+    //     { name: "Perl", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/perl/perl-original.svg" },
+    //     { name: "MATLAB", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/matlab/matlab-original.svg" }
+    // ];
+
+    // const framework = [
+    //     { name: "Ruby on Rails", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg" },
+    //     { name: "Laravel", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg" },
+    //     { name: "CakePHP", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cakephp/cakephp-original.svg" },
+    //     { name: "AngularJS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" },
+    //     { name: "Vue.js", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg" },
+    //     { name: "React", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
+    //     { name: "Bootstrap", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" },
+    //     { name: "Foundation", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/foundation/foundation-original.svg" },
+    //     { name: "Django", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" },
+    //     { name: "Spring", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" },
+    //     { name: "Gatsby", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gatsby/gatsby-plain.svg" },
+    //     { name: "Next.js", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
+    //     { name: "Nuxt.js", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg" },
+    //     { name: "Express", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" },
+    //     { name: "Flutter", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" }
+    // ];
+
+    // const other = [
+    //     { name: "PostgreSQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" },
+    //     { name: "MySQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" },
+    //     { name: "mongoDB", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" },
+    //     { name: "Heroku", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" },
+    //     { name: "Firebase", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" },
+    //     { name: "Github", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.sv" },
+    //     { name: "Docker", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" },
+    //     { name: "Slack", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-original.svg" },
+    //     { name: "HTML", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+    //     { name: "CSS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
+    //     { name: "SASS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" },
+    //     { name: "atom", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/atom/atom-original.svg" },
+    //     { name: "babel", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/babel/babel-original.svg" },
+    //     { name: "linux", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" }
+    // ];
     return { programmingLanguages, framework, other };
 }
--- app/category/page.jsx
+++ app/category/page.jsx
@@ -1,48 +1,209 @@
 'use client'
+import { useState, useEffect, useLayoutEffect, useRef } from "react"
 import { useRouter } from "next/navigation";
 import categoryDatas from "../Datas/categoryDatas";
 import style from "./page.module.scss"
 import Image from "next/image";
+import NoImage from "../../public/images/NoImage.png"
 import { useTagUpdateContext } from "../context/TagContext"
 
 export default function CategoryAll() {
-    const setAuth = useTagUpdateContext();
     const router = useRouter();
     const { programmingLanguages, framework, other } = categoryDatas();
     const categoryAll = [...programmingLanguages, ...framework, ...other];
-    const goTagPAge = (data) => {
-        setAuth(data.name)
+    const setTag = useTagUpdateContext();
+    const [keyword, setKeyword] = useState("")
+    const [filteredDatas, setFilteredDatas] = useState(categoryAll)
+    const [size, setSize] = useState(0)
+    const [ulRemainder, setUlRemainder] = useState("")
+    const ulRef = useRef(null)
+
+    const goTagPage = (data) => {
+        setTag(data.name)
         router.push(`/category/${data.name}/popular/1`)
     }
+
+    const categorySort = () => {
+        // 配列のソート
+        // 画面幅とアイテム数から、ulに付与する擬似要素のwidthを決定する
+        const resultRemainder = (r, d) => {
+            switch (r.length % d) {
+                case 0:
+                    console.log(`余り : 0`)
+                    setUlRemainder("ul_r0")
+                    break;
+                case 1:
+                    console.log(`余り : 1`)
+                    setUlRemainder("ul_r1")
+                    break;
+                case 2:
+                    console.log(`余り : 2`)
+                    setUlRemainder("ul_r2")
+                    break;
+                case 3:
+                    console.log(`余り : 3`)
+                    setUlRemainder("ul_r3")
+                    break;
+                case 4:
+                    console.log(`余り : 4`)
+                    setUlRemainder("ul_r4")
+                    break;
+                case 5:
+                    console.log(`余り : 5`)
+                    setUlRemainder("ul_r5")
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (keyword === "") {
+            setFilteredDatas(categoryAll);
+            console.log(`keywordが入力されてない: ${size}`)
+            const result = categoryAll
+
+            if (size >= 960) {
+                resultRemainder(result, 6)
+            } else if (size >= 768) {
+                resultRemainder(result, 4)
+            } else if (size >= 480) {
+                resultRemainder(result, 3)
+            } else {
+                resultRemainder(result, 2)
+            }
+
+            return;
+        }
+
+        const searchKeyword = keyword.trim().toLowerCase().match(/[^\s]+/g);
+        
+        if (searchKeyword === null) {
+            setFilteredDatas(categoryAll);
+            console.log(`入力ミスがある: ${size}`)
+            const result = categoryAll
+
+            if (size >= 960) {
+                resultRemainder(result, 6)
+            } else if (size >= 768) {
+                resultRemainder(result, 4)
+            } else if (size >= 480) {
+                resultRemainder(result, 3)
+            } else {
+                resultRemainder(result, 2)
+            }
+
+            return;
+        }
+
+        const result = categoryAll.filter((item) =>
+            searchKeyword.every((kw) => item.name.toLowerCase().indexOf(kw) !== -1)
+        );
+
+        if (result === 0) {
+            console.log("一致するタグがありませんでした")
+            return
+        }
+
+        setFilteredDatas(result.length ? result : ["NotFound Item"]);
+
+        // const resultRemainder = (r, d) => {
+        //     switch (r.length % d) {
+        //         case 0:
+        //             console.log(`余り : 0`)
+        //             setUlRemainder("ul_r0")
+        //             break;
+        //         case 1:
+        //             console.log(`余り : 1`)
+        //             setUlRemainder("ul_r1")
+        //             break;
+        //         case 2:
+        //             console.log(`余り : 2`)
+        //             setUlRemainder("ul_r2")
+        //             break;
+        //         case 3:
+        //             console.log(`余り : 3`)
+        //             setUlRemainder("ul_r3")
+        //             break;
+        //         case 4:
+        //             console.log(`余り : 4`)
+        //             setUlRemainder("ul_r4")
+        //             break;
+        //         case 5:
+        //             console.log(`余り : 5`)
+        //             setUlRemainder("ul_r5")
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // }
+
+        if (size >= 960) {
+            resultRemainder(result, 6)
+        } else if (size >= 768) {
+            resultRemainder(result, 4)
+        } else if (size >= 480) {
+            resultRemainder(result, 3)
+        } else {
+            resultRemainder(result, 2)
+        }
+    }
+
+    useEffect(() => {
+        setSize(window.innerWidth);
+    }, [])
+
+    useEffect(() => {
+        categorySort()
+    }, [keyword, size, ulRef])
+
+    useLayoutEffect(() => {
+        const updateSize = ()=> {
+            setSize(window.innerWidth);
+        };
+        window.addEventListener('resize', updateSize);
+        return () => window.removeEventListener('resize', updateSize);
+    }, [])
+
   
   return (
       <div className={style.category_page}>
           <h2 className={style.category_page_title}>カテゴリ一覧</h2>
-          
           <div className={style.category_sort_wrapper}>
               <label className={style.category_sort_label}>
-                <input className={style.category_sort} type="text" placeholder="検索"/>
+                <input className={style.category_sort} type="text" placeholder="検索" value={keyword} onChange={(e)=> {setKeyword(e.target.value)}} />
               </label>
-            </div>
+          </div>
           
-        <ul className={style.ul}>
-            {categoryAll.map((data, index) => {
-                return (
-                    <li className={style.li} key={index}>
-                        <Image
-                            src={data.src}
-                            alt="サイトロゴ"
-                            height={50}
-                            width={50}
-                            quality={90}
-                            priority={true}
-                            onClick={() => {goTagPAge(data)}}
-                        ></Image>
-                        <p className={style.li_name}>{ data.name }</p>    
-                    </li>
-                )
-            })}   
-        </ul>    
+          {filteredDatas == "NotFound Item" ? (
+              <p className={style.notFound}>アイテムが見つかりませんでした。</p>
+          ) : (null)}
+          <ul className={`${style.ul} ${style[ulRemainder]}`} ref={ulRef}>
+			  {/* ulは常に表示、dataが存在しないときはliを表示しない(dataを参照できなくてエラーが出る) */}
+			  	{filteredDatas == "NotFound Item" ? (
+					null
+			  	) : (
+				  filteredDatas.map((data, index) => {
+                    return (
+                        <li className={style.li} key={index}>
+                            <Image
+                                src={data.src}
+                                onError={(e) => {
+                                    e.target.onError = null;
+                                    e.target.src = NoImage;
+                                }}
+                                alt="サイトロゴ"
+                                height={50}
+                                width={50}
+                                quality={90}
+                                // priority={true}
+                                onClick={() => {goTagPage(data)}}
+                            ></Image>
+                            <p className={style.li_name}>{ data.name }</p>
+                        </li>
+                    )
+                	})
+				)}
+            </ul>    
     </div>
   )
 }
