b61049098d2f3e2750b0dab51b3c10069417a614
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-03-02 17:17:08 +0900

    ページ遷移時のスクロール位置を保持
    設定なしだと毎回最上部までスクロールさせるので位置を保持するように変更した

--- app/components/Pagenation/Pagenation.jsx
+++ app/components/Pagenation/Pagenation.jsx
@@ -1,30 +1,29 @@
 "use client"
-import { React, useState, useEffect } from 'react';
+import { React } from 'react';
 import Pagination from '@mui/material/Pagination';
 import { useRouter } from "next/navigation";
 import Stack from '@mui/material/Stack';
 import style from "./Pagenation.module.scss";
 import Articles from '../Articles/Articles';
 
+
 export default function Pagenation({ paramsId, sort, tag }) {
   const router = useRouter();
   const pageNumber = paramsId;
-  // const [pageNumber, setPageNumber] = useState(paramsId);
-  // console.log(tag)
 
   const routerPush = (e) => {
     const num = Number(e.target.textContent);
     if (tag == "") {
-      router.push(`/article/${sort}/${num}`);
+      router.push(`/article/${sort}/${num}`, { scroll: false });
     } else {
-      router.push(`/category/${tag}/${sort}/${num}`);
+      router.push(`/category/${tag}/${sort}/${num}`, { scroll: false });
     }
   }
 
   return (
     <section className="pagenation">
       <div className={style.container}>
-        <Articles pageNumber={pageNumber} sort={sort} tag={tag}></Articles>
+          <Articles pageNumber={pageNumber} sort={sort} tag={tag}></Articles>
         <span className={style.pagenation_button}>
           <Stack spacing={2}>
             <Pagination count={100} page={pageNumber} variant="outlined" shape="rounded" onClick={(e)=>routerPush(e)} hidePrevButton hideNextButton />
